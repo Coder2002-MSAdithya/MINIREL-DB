9c478c37a00079f1ee5c34fb4226a13010c89739
Adithya M S <rm224385@gmail.com>
2025-11-01 21:11:49 +0530

    Implemented Delete routine

--- algebra/delete.c
+++ algebra/delete.c
@@ -1,20 +1,118 @@
 #include "../include/defs.h"
 #include "../include/error.h"
 #include "../include/globals.h"
+#include "../include/helpers.h"
+#include "../include/openrel.h"
+#include "../include/findrec.h"
+#include "../include/deleterec.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 
-int Delete (int argc, char **argv)
+int Delete(int argc, char **argv)
 {
-    /* print command line arguments */
-    short k; /* iteration counter */
-    printf ("%s:\n", argv[0]);
-
-    for (k=1; k<argc; ++k)
+    if(!db_open)
     {
-        printf ("\targv[%d] = %s\n", k, argv[k]);
+        db_err_code = DBNOTOPEN;
+        return ErrorMsgs(db_err_code, print_flag);
     }
 
-    printf ("Delete \n");
-    return (OK); /* all's fine */
+    char *relName = argv[1];
+    char *attrName = argv[2];
+    int operator = *(int *)argv[3];
+    char *value = argv[4];
+
+    int recsAffected = 0;
+
+    if(strncmp(relName, RELCAT, RELNAME) == OK)
+    {
+        db_err_code = METADATA_SECURITY;
+        return ErrorMsgs(db_err_code, print_flag);
+    }
+
+    if(strncmp(relName, ATTRCAT, RELNAME) == OK)
+    {
+        db_err_code = METADATA_SECURITY;
+        return ErrorMsgs(db_err_code, print_flag);
+    }
+
+    int r = OpenRel(relName);
+
+    if(r == NOTOK)
+    {
+        db_err_code = RELNOEXIST;
+        return ErrorMsgs(db_err_code, print_flag);
+    }
+
+    bool found = false;
+    AttrDesc *ptr = catcache[r].attrList;
+    int recSize = catcache[r].relcat_rec.recLength;
+    
+    for(;ptr;ptr=ptr->next)
+    {
+        if(strncmp((ptr->attr).attrName, attrName, ATTRNAME) == OK)
+        {
+            found = true;
+            Rid recRid = INVALID_RID;
+            char type = (ptr->attr).type;
+            int offset = (ptr->attr).offset;
+            int size = (ptr->attr).length;
+            void *recPtr = malloc(recSize);
+            void *valuePtr = malloc(size);
+
+            if(type == 'i')
+            {
+                if(!isValidInteger(value))
+                {
+                    db_err_code = INVALID_VALUE;
+                    return ErrorMsgs(db_err_code, print_flag);
+                }
+
+                *(int *)valuePtr = atoi(value);
+            }
+            else if(type == 'f')
+            {
+                if(!isValidFloat(value))
+                {
+                    db_err_code = INVALID_VALUE;
+                    return ErrorMsgs(db_err_code, print_flag);
+                }
+
+                *(float *)valuePtr = atof(value);
+            }
+            else if(type == 's')
+            {
+                strncpy(valuePtr, value, size);
+                ((char *)valuePtr)[size - 1] = '\0';
+            }
+
+            do
+            {
+                FindRec(r, recRid, &recRid, recPtr, type, size, offset, valuePtr, operator);
+
+                if(!isValidRid(recRid))
+                {
+                    break;
+                }
+
+                DeleteRec(r, recRid);
+                recsAffected++;
+            } 
+            while(true);
+
+            free(recPtr);
+            free(valuePtr);
+        }
+    }
+
+    if(!found)
+    {
+        db_err_code = ATTRNOEXIST;
+        return ErrorMsgs(db_err_code, print_flag);
+    }
+
+    printf("%d records successfully deleted from relation %s\n", recsAffected, relName);
+
+    return OK;
 }
