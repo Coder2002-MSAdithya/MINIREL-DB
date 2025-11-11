13be0746952aef8646bdd7260ebd67ae6e68d9d9
Adithya M S <rm224385@gmail.com>
2025-11-11 23:12:24 +0530

    Moved helper function to create relation with a linked list of attributes to helpers.c

--- include/helpers.h
+++ include/helpers.h
@@ -11,6 +11,7 @@ int writeRecsToFile(const char *, void *, int, int, char);
 int float_cmp(double a, double b, double rel_eps, double abs_eps);
 double dmax(double, double);
 Rid IncRid(Rid rid, int recsPerPg);
+int CreateFromAttrList(const char *dstRelName, AttrDesc *head);
 int FreeLinkedList(void **headPtr, size_t nextOff);
 bool isValidRid(Rid rid);
 bool isValidFloat(char *str);
