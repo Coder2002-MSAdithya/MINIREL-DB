ee23025eb0467cf6e8c596be488588f1fb22b92a
Adithya M S <rm224385@gmail.com>
2025-11-29 13:15:23 +0530

    Implemented a new routine to create a new relation with exact same attribute names and offsets

---
+++ include/createrel.h
@@ -0,0 +1,3 @@
+#ifndef _CREATE_REL_H
+int CreateRel(char *relName, int relNum);
+#endif
---
+++ physical/createrel.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+#include "../include/error.h"
+#include "../include/defs.h"
+#include "../include/globals.h"
+#include "../include/insertrec.h"
+
+/* Create a new relation with name relName with same attribute descriptors as relNum */
+int CreateRel(char *relName, int relNum)
+{
+    RelCatRec new_rc = catcache[relNum].relcat_rec;
+    new_rc.numPgs = new_rc.numRecs = 0;
+    AttrDesc *ptr = catcache[relNum].attrList;
+    strncpy(new_rc.relName, relName, RELNAME);
+
+    FILE *fptr = fopen(relName, "w");
+
+    if(!fptr)
+    {
+        db_err_code = FILESYSTEM_ERROR;
+        return NOTOK;
+    }
+
+    InsertRec(RELCAT_CACHE, &new_rc);
+
+    for(;ptr;ptr=ptr->next)
+    {
+        AttrCatRec ac = ptr->attr;
+        strncpy(ac.relName, relName, RELNAME);
+        ac.hasIndex = 0;
+        ac.nKeys = ac.nPages = 0;
+        InsertRec(ATTRCAT_CACHE, &ac);
+    }
+
+    return OK;
+}
