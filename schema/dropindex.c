59e25cd15712a6156b8dd09ae328117d488da6cb
Adithya M S <rm224385@gmail.com>
2025-11-29 13:17:18 +0530

    Added basic error handling code in DropIndex routine

--- schema/dropindex.c
+++ schema/dropindex.c
@@ -1,20 +1,48 @@
 #include "../include/defs.h"
 #include "../include/error.h"
 #include "../include/globals.h"
+#include "../include/openrel.h"
+#include "../include/findrelattr.h"
 #include <stdio.h>
 
 
 int DropIndex (int argc, char **argv)
 {
-    /* print command line arguments */
-    short k; /* iteration counter */
-    printf ("%s:\n", argv[0]);
-    
-    for (k=1; k<argc; ++k)
+    if(!db_open)
     {
-        printf ("\targv[%d] = %s\n", k, argv[k]);
+        db_err_code = DBNOTOPEN;
+        return ErrorMsgs(db_err_code, print_flag);
     }
 
-    printf ("DropIndex \n");
-    return (OK); /* all's fine */
+    char *relName = argv[1];
+    char *attrName = argc == 3 ? argv[2] : NULL;
+
+    AttrDesc *attrPtr = NULL;
+
+    int r = OpenRel(relName);
+
+    if(r == NOTOK)
+    {
+        return ErrorMsgs(db_err_code, print_flag);
+    }
+
+    if(argc == 3)
+    {
+        attrPtr = FindRelAttr(r, attrName);
+
+        if(!attrPtr)
+        {
+            db_err_code = ATTRNOEXIST;
+            return ErrorMsgs(db_err_code, print_flag);
+        }
+    }
+
+    if(attrPtr)
+    {
+        // Remove ONLY index on that attribute ...
+    }
+    else
+    {
+        // Remove all indices on relation r ...
+    }
 }
